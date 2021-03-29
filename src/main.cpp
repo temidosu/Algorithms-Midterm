ae9a720c763e2c2aef9c3c76d34d4480e9c5a8e6
temidosu <tdosu@smu.edu>
2021-03-29 11:32:16 -0500

    Folly built and run

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
+#include <folly/FBString.h>
 #include <iostream>
-#include <string>
+#include <string> 
 
 int main(){
 	printf("Libc version is %d.%d\n", __GLIBC__, __GLIBC_MINOR__);
+    folly::fbstring str = "hello, world!";
+    std::cout << str << std::endl;
+    return 0;
 }
